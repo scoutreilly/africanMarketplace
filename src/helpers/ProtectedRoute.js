67cd40db742bd750de6eacc50cb3beb1c7d7912a
tamara-taylor <ttaylor8522@gmail.com>
2021-09-25 15:21:20 -0400

    updated

--- src/helpers/ProtectedRoute.js
+++ src/helpers/ProtectedRoute.js
@@ -4,17 +4,14 @@ import { Route, Redirect } from "react-router";
 // If the user is authenticated, render the "component" prop (normal behavior)
 // Otherwise, redirect to /login
 
-const ProtectedRoute = ({ component, ...rest }) => {
-  if (localStorage.getItem('token')) {
-    return (
-      <Route component={component} {...rest} />
-    )
-  }
-  else {
-    return (
-      <Redirect to="/login" />
-    )
-  }
-};
+const ProtectedRoute = ({component, ...rest}) => {
+  return <Route {...rest} render={(props) => {
+      if(localStorage.getItem('token')) {
+          return(<component {...props} />)
+      }else {
+          return <Redirect to='/login' />
+      }
+  }} />
+}
 
 export default ProtectedRoute;
