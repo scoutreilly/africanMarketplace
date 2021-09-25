1eef8281e86b04619e76b6cce4ffe29e6a498427
scoutreilly <scouty33@gmail.com>
2021-09-24 21:38:24 -0600

    login styles

---
+++ src/components/StyledComponents/InputStyled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  padding: 0.5em;
+  color: #073421;
+  background: fff;
+  border: none;
+  border-radius: 3px;
+  width: 100%;
+  margin-bottom: 1.5em;
+`;
+
+export default Input;
---
+++ src/components/StyledComponents/LabelStyled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Label = styled.label`
+  margin-bottom: 1.5em;
+  display: block;
+  color: #2d2232;
+`;
+
+export default Label;
